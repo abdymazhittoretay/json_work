d0c743752a008f5001af7c90f18d3e5c81ef9ae6
Toretay <abdymvzhit@gmail.com>
2025-02-26 17:12:39 +0500

    fetched data from openweatherapi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:json_work/secrets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,6 +23,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    getData().catchError((e) {
+      print(e);
+    });
+    super.initState();
+  }
+
+  Future<void> getData() async {
+    var url = Uri.parse(
+      "https://api.openweathermap.org/data/2.5/weather?q=Shymkent&appid=$apiKey",
+    );
+
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(backgroundColor: Colors.white);
